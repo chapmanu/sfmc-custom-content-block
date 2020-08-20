6c3c34e3f1b8fc8142e39ad36020c8840939315c
Chris <chrs.bryant@gmail.com>
2020-08-19 20:20:50 -0700

    added port option for server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000
 
 app.use('/', express.static('dist'));
-app.listen(3000, () => console.log('App listening on 3000'));
+app.listen(port, () => console.log(`App listening on ${port}`));
